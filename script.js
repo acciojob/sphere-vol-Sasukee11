874fa81f898ae1b2b2b765458ce54533945f4143
Sasuke <118628015+Sasukee11@users.noreply.github.com>
2023-07-17 03:29:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ event.preventDefault(); // Prevent form submission
   const radius = parseFloat(radiusInput.value);
   const volume = (4/3) * Math.PI * Math.pow(radius, 3);
 
-  volumeInput.value = volume;
+  volumeInput.value = volume.toFixed(4);
   
 } 
 
